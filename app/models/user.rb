672fcfbc9472648754f618332c09189448ba9fcd
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-01-13 00:00:25 -0500

    Modelling users

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	validates(:name, presence: true, length: { maximum: 50 })
+	REGEX_VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates(:email, presence: true, length: { maximum: 255 }, format: { with: REGEX_VALID_EMAIL }, 
+			uniqueness: { case_sensitive: false })
+
+	has_secure_password
+        validates :password, presence: true, length: { minimum: 6 }
+end
